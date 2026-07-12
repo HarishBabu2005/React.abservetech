441c1d86464d526954e367f4a5009b3f1208cd75
Harishbabu2005 <harishbabups2005@gmail.com>
2026-07-12 11:55:40 +0530

    real form submission is completed

--- vite_project/src/Components/Form-demo/StudentForm.jsx
+++ vite_project/src/Components/Form-demo/StudentForm.jsx
@@ -14,18 +14,40 @@ const StudentForm = () => {
       
     });
     
+    
   }
+   const handleSubmit=(e)=>{
+   
+    e.preventDefault();
+    console.log("Successfully Registered",Student)
+    SetStudent({
+      name:"",
+      dep:"",
+      clg:""
+
+    })
+          
+          
+         
+          
+   }
  
   return (
+    <>
     <div>
         <h1>Student Form</h1>
         <h3>Name:{Student.name}</h3>
         <h3>Department: {Student.dep}</h3>
         <h3>College: {Student.clg}</h3>
-        <input placeholder='enter the name' name='name' value={Student.name} type='text' onChange={handleChange}></input>
-        <input placeholder='enter the department' name='dep' value={Student.dep} type='text' onChange={handleChange}></input>
-        <input placeholder='enter the College' name='clg' value={Student.clg} type='text' onChange={handleChange}></input>
+        <form onSubmit={handleSubmit}>
+        <input placeholder='enter the name' name='name' value={Student.name} type='text' required onChange={handleChange} ></input>
+        <input placeholder='enter the department' name='dep' value={Student.dep} type='text' required onChange={handleChange}></input>
+        <input placeholder='enter the College' name='clg' value={Student.clg} type='text' required onChange={handleChange}></input>
+        <button>submit</button>
+      </form>
     </div> 
+    
+    </>
   )
 }
 
