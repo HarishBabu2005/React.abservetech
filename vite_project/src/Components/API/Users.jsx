50c20ffe237ab2b646ac68395f8079abd1b8602f
Harishbabu2005 <harishbabups2005@gmail.com>
2026-07-14 15:26:55 +0530

    axios completed

--- vite_project/src/Components/API/Users.jsx
+++ vite_project/src/Components/API/Users.jsx
@@ -1,17 +1,26 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
+import api from "./Axios"
  const Users = () => {
     const [users,setUsers]=useState([])
     useEffect(()=>{
+        //  async function getUsers(){
+        //       const res=  await fetch("https://jsonplaceholder.typicode.com/users")
+        //   const data =await res.json();
+        //   setUsers(data)
+         
+        //  }
+        //  getUsers()
          async function getUsers(){
-              const res=  await fetch("https://jsonplaceholder.typicode.com/users")
-          const data =await res.json();
+              const res=  await api.get("/users")
+          const data =res.data
           setUsers(data)
          
          }
          getUsers()
           
     },[])
+
   return (
     <div>Users
         <ul>
