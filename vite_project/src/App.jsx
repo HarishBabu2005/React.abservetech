44edc5997060af651b0701d4816d467c582e08a9
Harishbabu2005 <harishbabups2005@gmail.com>
2026-07-19 22:49:08 +0530

    protectedroute and logout functionality is done

--- vite_project/src/App.jsx
+++ vite_project/src/App.jsx
@@ -21,6 +21,7 @@ import Setting from "./pages/NestedRoute/Setting";
 import Users from "./Components/API/Users";
 import UserForm from "./Components/Form-demo/UserForm";
 import LoginForm from "./Components/Authentication/LoginForm";
+import ProtectedRoute from "./pages/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -34,22 +35,28 @@ function App() {
       {/* <Parent name="Harish"/> */}
       {/* <Form/> */}
       {/* <StudentForm/> */}
-      
-        <Routes>
-          <Route path="/login" element={ <LoginForm/>}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/student/:id" element={<Student/>}></Route>
-          <Route path="/*" element={<NotFound/>}/>
-          <Route path="/dashboard" element={<DashBoard/>}>
-              <Route path="profile" element={<Profile/>}></Route>
-              <Route path="setting" element={<Setting/>}></Route>
-          </Route>
-        </Routes>
-        {/* <Users/> */}
-        {/* <UserForm/> */}
-     
+
+      <Routes>
+        <Route path="/login" element={<LoginForm />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/student/:id" element={<Student />}></Route>
+        <Route path="/*" element={<NotFound />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="profile" element={<Profile />}></Route>
+          <Route path="setting" element={<Setting />}></Route>
+        </Route>
+      </Routes>
+      {/* <Users/> */}
+      {/* <UserForm/> */}
     </>
   );
 }
--- vite_project/src/pages/NestedRoute/DashBoard.jsx
+++ vite_project/src/pages/NestedRoute/DashBoard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const DashBoard = () => {
+const navigate=useNavigate()
+const handleLogout = () => {
+    const confirmLogout = window.confirm(
+        "Are you sure you want to logout?"
+    );
+
+    if (!confirmLogout) return;
+
+    localStorage.removeItem("token");
+
+    navigate("/login", { replace: true });
+};
   return (
     <div>
+      <div style={{display:"flex", flexDirection:"row-reverse"}}>
+        <Button onClick={handlelogout}>Log out</Button>
+      </div>
         DashBoard
         <Outlet/>
     </div>
