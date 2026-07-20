77cb8c7fffecd8e368c80bfedda31229fc7a0c23
Harishbabu2005 <harishbabups2005@gmail.com>
2026-07-20 20:51:21 +0530

    show logges user name

--- vite_project/src/pages/NestedRoute/DashBoard.jsx
+++ vite_project/src/pages/NestedRoute/DashBoard.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { Outlet, useNavigate } from 'react-router-dom'
+import api from "../../Components/API/Axios"
 
 const DashBoard = () => {
+   const [user,setUser]=useState({});
 const navigate=useNavigate()
+const fetchUser=async()=>{
+  try {
+    const res=await api.get("/users/profile");
+  console.log(res.data.user)
+  setUser(res.data.user);
+  } catch (e) {
+    console.error(e)
+  }
+  
+}
+useEffect(()=>{
+     fetchUser();
+},[]);
 const handleLogout = () => {
+ 
     const confirmLogout = window.confirm(
         "Are you sure you want to logout?"
     );
@@ -18,9 +35,10 @@ const handleLogout = () => {
   return (
     <div>
       <div style={{display:"flex", flexDirection:"row-reverse"}}>
-        <Button onClick={handlelogout}>Log out</Button>
+        <Button onClick={handleLogout}>Log out</Button>
       </div>
         DashBoard
+        <h2>Welcome,{user.name}</h2>
         <Outlet/>
     </div>
   )
